c8a56627a2c09f6096fe182b00f70e764d94b432
a21frroje <96067489+a21frroje@users.noreply.github.com>
2023-09-19 09:55:42 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Jesús Frías Rojas</title>
+</head>
+<body>
+	<img src="./img/perfil.jpg" />
+	<h1>JESUS FRIAS ROJAS</h1>
+	<a href="./unidad1/index.php">UNIDAD 1</a><br>
+	<a href="">UNIDAD 2</a><br>
+	<a href="">UNIDAD 3</a><br>
+	<a href="">UNIDAD 4</a><br>
+</body>
+</html>
